fdb0d106dc7aa6566ecc34abd96de1e0c724d998
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-04-17 22:23:51 +0545

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,33 @@
+import asyncio
+from pyrogram import idle, Client
+from SURUCHIXMUSIC.clients import bot, assistant, call 
+import config
+
+async def start_bot():
+    print("🚀 Starting SuruchiXmusic Clients...")
+    
+    # --- PLUGINS LOADING LOGIC ---
+    # Ye batata hai bot ko ki commands kahan rakhi hain
+    bot.plugins = {"root": "SURUCHIXMUSIC/plugins"} 
+    
+    await bot.start()
+    await assistant.start()
+    await call.start()
+    
+    print("---------------------------------")
+    print("✨ SURUCHIXMUSIC IS NOW ONLINE! ✨")
+    print("✅ ALL MODULES LOADED")
+    print("---------------------------------")
+    
+    await idle()
+    
+    # Stopping clients on exit
+    await bot.stop()
+    await assistant.stop()
+    await call.stop()
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_until_complete(start_bot())
+    except KeyboardInterrupt:
+        print("\n🛑 Bot Stopped.")
