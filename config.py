417e0425a7d65dc98eabdee8a50c70391a5f8514
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-04-19 22:22:43 +0545

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from datetime import datetime
 # --- Bot Credentials ---
 API_ID = 39005479
 API_HASH = "c2e7265b2e96025adcc4731c2c1e5ba5"
-BOT_TOKEN = "8534739129:AAEY1_SiskC73NPMBq3IDU8XfYCbVuZWbsE"
-SESSION_STRING = "BQJTLScAnOFYKCwnyrSjEUcnuqVN7bm85oVmteMpak2s4XL7Hb0yQ04kwaMAMtPZqSUUJaCr97887jGaR8LlEX8zocmr5Mutz7tXGOKUnCDNWxX1KHvkbRczcdMaQHFHFxO1mxCvdOx8abVkTpcV5IyTnlO6GO6IwFiISPYTRF3ekCiZFWhYC0oH3ZzFRMVXtSAMFH8G1rxKwkmn_ooOXNJZdoTNfMzv4_WU_Jusp8ASALGU3WV30Ugn8UEtYm2glTo0V9nuOGv_ZF7zQA-trkQjRnKM17JGTxDFVvk9LZSF03ekSV7cAdje4dJM_XVHwyewU3cRX4zzfpE3CJ3e_jqQG0iKDwAAAAIKSZQTAA"
+BOT_TOKEN = "8596765113:AAGlK4eW_0Qxlt6hfD_ElFcwronZivnNM6w"
+SESSION_STRING = "BQE-4i0AclIM1MxvSfSiIlXqBAZNJttV9NpXiIjzKpNtQ-7iwCF_UtL_PeeTIWlVtyJJiJDbPP8AMxLiTf8HQcXhYl1D8e1fiLdJB6VSZpdnbLSfVUjGs_8px-rhz0Ql99f1EiEjcFcgUbQtTuwaw_XSHPMbxV-_z69F7XzrmN9Z0kQKEljA6O7YTn2TfnjF9wIWICWmnP2I9AVybQmhDvKDEge_6GY7e0a59qVXaLHRFMj3iUTt9ca0ldOg43J5NHakn0pFXkqyuGXvM60hubcynP7WXCLOiuc1Ct0GD3gjBxRS6EtNtccWK8EpksJCCC70dIb7vp_Y5l0u1eZ2JjVkGyPyagAAAAH-nwzIAA"
 # --- Random image links ---" # Apni session string yahan puri daal dena
-OWNER_ID = 7724452546
+OWNER_ID = 8566803656
 # --- Global Tracking ---
 queues = {}
 playing_messages = {} # {chat_id: message_id} - Timer edit karne ke liye
