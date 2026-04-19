c5d79652ff57732bd3aaadf3f29ae05649372fa9
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-04-19 21:40:17 +0545

    Initialize bot and assistant clients
    Added bot and assistant clients for music functionality.

--- SURUCHIMUSIC/clients.py
+++ SURUCHIMUSIC/clients.py
@@ -1,1 +1,23 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+import config
 
+# Bot Client
+bot = Client(
+    "ARUMUSIC_BOT",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+    plugins=dict(root="ARUMUZIC/plugins")
+)
+
+# Assistant Client
+assistant = Client(
+    "ARUMUSIC_ASS",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    session_string=config.SESSION_STRING
+)
+
+# Music Engine
+call = PyTgCalls(assistant)
